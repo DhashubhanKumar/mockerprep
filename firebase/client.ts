8e94b006732eac03ace2a2ad29c3f8d15e15a217
DhashubhanKumar <dhashupersonal@gmail.com>
2025-04-10 17:48:34 +0530

    added extra security feature

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,7 +9,7 @@ import { getFirestore, initializeFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = { 
-  apiKey: "AIzaSyD4BoLPNC8NAFNT5q71zA8qkgCCbToY2tc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "mockerprep.firebaseapp.com",
   projectId: "mockerprep",
   storageBucket: "mockerprep.firebasestorage.app",
