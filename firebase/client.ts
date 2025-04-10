9c9c897c8ba2b776608f0708fe02283484c96f4a
DhashubhanKumar <dhashupersonal@gmail.com>
2025-04-10 17:45:54 +0530

    fixed the auth error

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,25 +2,29 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+const firebaseConfig = { 
+  apiKey: "AIzaSyD4BoLPNC8NAFNT5q71zA8qkgCCbToY2tc",
+  authDomain: "mockerprep.firebaseapp.com",
+  projectId: "mockerprep",
+  storageBucket: "mockerprep.firebasestorage.app",
+  messagingSenderId: "734928583319",
+  appId: "1:734928583319:web:3663210409c7be345e07d2",
+  measurementId: "G-Z9KT9C3KM2"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
+
+// Enable ignoreUndefinedProperties
+initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+});
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
